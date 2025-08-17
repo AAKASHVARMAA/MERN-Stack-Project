d2ea16d5daf5544a908291497da48668d9a57811
akash <akashkarnekari@gmail.com>
2025-08-17 20:30:16 +0530

    updated app.js

--- Video_Conferensing_web_App/Backend/src/app.js
+++ Video_Conferensing_web_App/Backend/src/app.js
@@ -15,7 +15,7 @@ const io=ConnectToServer(server)
 
 app.set("port",(process.env.PORT || 8000))
 app.use(cors())
-app.use(express.json({"limit":"40kb"}))
+app.use(express.json({"limit":"30kb"}))
 app.use(express.urlencoded({"limit":"40kb", extended:"true"}))
 
 
